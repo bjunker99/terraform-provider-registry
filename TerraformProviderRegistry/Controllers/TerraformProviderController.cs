28cbb1af96d2fd3462aa2632daf5559127aa6bda
bjunker <80123850+bjunker99@users.noreply.github.com>
2022-03-23 12:43:44 -0700

    Authentication fix
    Fixed uncaught error on authentication

--- TerraformProviderRegistry/Controllers/TerraformProviderController.cs
+++ TerraformProviderRegistry/Controllers/TerraformProviderController.cs
@@ -21,10 +21,19 @@ namespace TerraformProviderRegistry.Controllers
         [HttpGet("{ns}/{name}/{version}/download/{os}/{arch}")]
         public async Task<IActionResult> ProviderPackage(string ns, string name, string version, string os, string arch)
         {
-            var token = TerraformAuthenticationService.ValidateJWTToken(ControllerContext.HttpContext.Request.Headers.Authorization, _config.TOKEN_SECRET_KEY);
+            if (_config.AUTHENTICATION_ENABLED)
+            {
+                try
+                {
+                    var token = TerraformAuthenticationService.ValidateJWTToken(ControllerContext.HttpContext.Request.Headers.Authorization, _config.TOKEN_SECRET_KEY);
 
-            if (_config.AUTHENTICATION_ENABLED && token == null)
-                return Unauthorized();
+                    if (token == null)
+                        return Unauthorized();
+                } catch (Exception)
+                {
+                    return Unauthorized();
+                }
+            }
 
             var tps = new TerraformProviderService(_config.TERRAFORM_PROVIDER_BUCKET);
             string? response = await tps.ProviderPackage(ns, name, version, os, arch);
@@ -38,10 +47,19 @@ namespace TerraformProviderRegistry.Controllers
         [HttpGet("{ns}/{name}/versions")]
         public async Task<IActionResult> Versions(string ns, string name)
         {
-            var token = TerraformAuthenticationService.ValidateJWTToken(ControllerContext.HttpContext.Request.Headers.Authorization, _config.TOKEN_SECRET_KEY);
+            if (_config.AUTHENTICATION_ENABLED)
+            {
+                try
+                {
+                    var token = TerraformAuthenticationService.ValidateJWTToken(ControllerContext.HttpContext.Request.Headers.Authorization, _config.TOKEN_SECRET_KEY);
 
-            if (_config.AUTHENTICATION_ENABLED && token == null)
-                return Unauthorized();
+                    if (token == null)
+                        return Unauthorized();
+                } catch (Exception)
+                {
+                    return Unauthorized();
+                }
+            }
 
             var tps = new TerraformProviderService(_config.TERRAFORM_PROVIDER_BUCKET);
             string? response = await tps.Versions(ns, name);
