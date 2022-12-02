8556a990e701d554bef7dcff59de34b95210eee2
bjunker <80123850+bjunker99@users.noreply.github.com>
2022-12-01 19:35:47 -0800

    Update Program.cs

--- TerraformProviderRegistry/Program.cs
+++ TerraformProviderRegistry/Program.cs
@@ -8,13 +8,17 @@ builder.Services.AddControllers();
 IServiceConfiguration serviceConfig = new ServiceConfiguration();
 builder.Services.AddSingleton(serviceConfig);
 
-var app = builder.Build();
-
 string? aws_region = Environment.GetEnvironmentVariable("AWS_REGION");
 
 if (aws_region != null)
 {
     builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+}
+
+var app = builder.Build();
+
+if (aws_region != null)
+{
     app.UseHttpsRedirection();
 }
 
